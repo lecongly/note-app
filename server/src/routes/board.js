295526e724a7650c3e659f5ab837f72b9d69ba4c
Lê Công Lý <58856636+lecongly@users.noreply.github.com>
2022-12-16 21:51:35 +0700

    feat(server): board api get, update, delete a board, get & update position favourites

--- server/src/routes/board.js
+++ server/src/routes/board.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const {param} = require('express-validator')
+const validation = require('../handlers/validation')
 const tokenHandler = require('../handlers/tokenHandler')
 const boardController = require('../controllers/board')
 
@@ -19,5 +21,55 @@ router.put(
     tokenHandler.verifyToken,
     boardController.updatePosition
 )
+
+router.get(
+    '/favourites',
+    tokenHandler.verifyToken,
+    boardController.getFavourites
+)
+
+router.put(
+    '/favourites',
+    tokenHandler.verifyToken,
+    boardController.updateFavouritePosition
+)
+
+
+router.get(
+    '/:boardId',
+    param('boardId').custom(value => {
+        if (!validation.isObjectId(value)) {
+            return Promise.reject('invalid id')
+        } else return Promise.resolve()
+    }),
+    validation.validate,
+    tokenHandler.verifyToken,
+    boardController.getOne
+)
+
+router.put(
+    '/:boardId',
+    param('boardId').custom(value => {
+        if (!validation.isObjectId(value)) {
+            return Promise.reject('invalid id')
+        } else return Promise.resolve()
+    }),
+    validation.validate,
+    tokenHandler.verifyToken,
+    boardController.update
+)
+
+router.delete(
+    '/:boardId',
+    param('boardId').custom(value => {
+        if (!validation.isObjectId(value)) {
+            return Promise.reject('invalid id')
+        } else return Promise.resolve()
+    }),
+    validation.validate,
+    tokenHandler.verifyToken,
+    boardController.delete
+)
+
 
 module.exports = router
